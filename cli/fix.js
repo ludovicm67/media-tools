94008e80061f6b01f28987266ce4db8233730c49
Ludovic Muller <ludovicmuller1@gmail.com>
2023-11-01 16:46:12 +0100

    refactor: move the fix command into a dedicated file

---
+++ cli/fix.js
@@ -0,0 +1,55 @@
+// @ts-check
+import { writeFileSync } from 'node:fs';
+import { readFile } from 'node:fs/promises';
+import { decode, resetDecoder, displayDecodedElements } from '../lib/ebml.js';
+
+/**
+ * Fix a WebM file using the previous chunk.
+ * The previous chunk should be a sane chunk.
+ * It should be the one that is right before the broken chunk.
+ *
+ * @param {string} prevChunkPath The path to the previous (sane) chunk.
+ * @param {string} brokenChunkPath The path to the broken chunk.
+ * @param {string} outputPath The path where to write the fixed chunk.
+ * @param {Record<string, any>} options options from the CLI.
+ */
+const fix = async (prevChunkPath, brokenChunkPath, outputPath, options) => {
+  const debug = options.debug;
+  if (debug) {
+    console.info('Debug mode enabled');
+    console.log(`> Previous chunk path: ${prevChunkPath}`);
+    console.log(`> Broken chunk path: ${brokenChunkPath}`);
+    console.log(`> Output path: ${outputPath}`);
+    console.log('');
+  }
+
+  const prevChunk = await readFile(prevChunkPath);
+  const brokenChunk = await readFile(brokenChunkPath);
+
+  resetDecoder({
+    debug: false,
+  });
+  const { decoded, headerBuffer, lastStartBuffer } = decode(prevChunk);
+  if (debug) {
+    console.info("\nDecoded previous chunk:");
+    displayDecodedElements(decoded);
+    console.log('');
+  }
+
+  const newFile = Buffer.concat([headerBuffer, lastStartBuffer, brokenChunk]);
+  resetDecoder({
+    debug: false,
+  });
+  const { decoded: newFileDecoded } = decode(newFile);
+  if (debug) {
+    console.info("\nDecoded fixed chunk:");
+    displayDecodedElements(newFileDecoded);
+    console.log('');
+
+    console.info(`\nWriting fixed chunk to file: '${outputPath}'`);
+  }
+
+  writeFileSync(outputPath, newFile)
+}
+
+export default fix;
--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,7 @@
 // @ts-check
-import { writeFileSync } from 'node:fs';
-import { readFile } from 'node:fs/promises';
 import { Command } from 'commander';
-import { decode, resetDecoder, displayDecodedElements } from '../lib/ebml.js';
 import info from '../package.json' assert { type: 'json' };
+import fix from './fix.js';
 
 const program = new Command();
 
@@ -18,43 +16,6 @@ program.command('fix')
   .argument('<broken chunk>', 'path to the WebM file to fix (broken chunk)')
   .argument('<output path>', 'path to export the fixed WebM chunk to')
   .option('--debug', 'show debug information')
-  .action(async (prevChunkPath, brokenChunkPath, outputPath, options) => {
-    const debug = options.debug;
-    if (debug) {
-      console.info('Debug mode enabled');
-      console.log(`> Previous chunk path: ${prevChunkPath}`);
-      console.log(`> Broken chunk path: ${brokenChunkPath}`);
-      console.log(`> Output path: ${outputPath}`);
-      console.log('');
-    }
-
-    const prevChunk = await readFile(prevChunkPath);
-    const brokenChunk = await readFile(brokenChunkPath);
-
-    resetDecoder({
-      debug: false,
-    });
-    const { decoded, headerBuffer, lastStartBuffer } = decode(prevChunk);
-    if (debug) {
-      console.info("\nDecoded previous chunk:");
-      displayDecodedElements(decoded);
-      console.log('');
-    }
-
-    const newFile = Buffer.concat([headerBuffer, lastStartBuffer, brokenChunk]);
-    resetDecoder({
-      debug: false,
-    });
-    const { decoded: newFileDecoded } = decode(newFile);
-    if (debug) {
-      console.info("\nDecoded fixed chunk:");
-      displayDecodedElements(newFileDecoded);
-      console.log('');
-
-      console.info(`\nWriting fixed chunk to file: '${outputPath}'`);
-    }
-
-    writeFileSync(outputPath, newFile)
-  });
+  .action(fix);
 
 program.parse(process.argv);
