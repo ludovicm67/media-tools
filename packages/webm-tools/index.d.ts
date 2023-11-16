94ec2557e94202254942fcec54d8a2d023efb1a6
Ludovic Muller <ludovicmuller1@gmail.com>
2023-11-16 18:42:17 +0100

    webm-tools: generate types

---
+++ packages/webm-tools/index.d.ts
@@ -0,0 +1,5 @@
+export { Buffer } from "buffer/index.js";
+export * as utils from "./lib/utils.js";
+export { default as ebmlSchema } from "./lib/ebml-schema.js";
+export * as ebml from "./lib/ebml.js";
+//# sourceMappingURL=index.d.ts.map
