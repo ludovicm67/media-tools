0c65f6a34dd4bc32f33f312386ae92470707dacb
Ludovic Muller <ludovicmuller1@gmail.com>
2023-11-30 12:46:38 +0100

    chore: remove .d.ts files

--- packages/webm-tools/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { Buffer } from "buffer/index.js";
-export * as utils from "./lib/utils.js";
-export { default as ebmlSchema } from "./lib/ebml-schema.js";
-export * as ebml from "./lib/ebml.js";
-//# sourceMappingURL=index.d.ts.map
